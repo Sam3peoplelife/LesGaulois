2714bc51db0d646a460cfced759291e972e8a94c
Sam3peoplelife <kamishartem1@gmail.com>
2023-09-21 10:25:07 +0200

    Importting fckn package

---
+++ src/historie/Scenario.java
@@ -0,0 +1,10 @@
+package historie;
+import personnages.*;
+public class Scenario {
+
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("New Wor", 50);
+		asterix.parler("Get out of my house");
+	}
+
+}
