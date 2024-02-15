ef4320e7cec702a76382b6237fc86a5b3b99c4f5
Sam3peoplelife <kamishartem1@gmail.com>
2024-02-15 10:58:22 +0100

    TP1 from 0

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,27 +1,50 @@
 package personnages;
 
 public class Druide {
-	
-	private String nom;
-	private int effetPotionMin;
-	private int effetPotionMax;
-	
-	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
-		this.nom = nom;
-		this.effetPotionMin = effetPotionMin;
-		this.effetPotionMax = effetPotionMax;
-		parler("Bonjour, je suis le druide " + nom + " et ma potion peut allerd'une force " + effetPotionMin + " à " + effetPotionMax + ".");
-	}
-	
-	public String getNom() {
-		return nom;
-	}
-	
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "« " + texte + "»");
-	}
-	
-	private String prendreParole() {
-		return "Le druide " + nom + " : ";
-	}
+    private String nom;
+    private int effetPotionMin;
+    private int effetPotionMax;
+    private int forcePotion;
+
+    public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+        this.nom = nom;
+        this.effetPotionMin = effetPotionMin;
+        this.effetPotionMax = effetPotionMax;
+        parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à "
+                + effetPotionMax + ".");
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "« " + texte + "»");
+    }
+
+    private String prendreParole() {
+        return "Le druide " + nom + " : ";
+    }
+
+    public void preparerPotion() {
+        // Implementation of preparerPotion method as before
+    }
+
+    public void booster(Gaulois gaulois) {
+        if (gaulois.getNom().equals("Obélix")) {
+            parler("Non, Obélix !... Tu n’auras pas de potion magique !");
+        } else {
+            gaulois.boirePotion(forcePotion);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test booster method
+        Druide panoramix = new Druide("Panoramix", 5, 10);
+        Gaulois asterix = new Gaulois("Asterix", 8);
+        Gaulois obelix = new Gaulois("Obélix", 10);
+
+        panoramix.booster(asterix); // Asterix receives potion
+        panoramix.booster(obelix); // Obelix is denied potion
+    }
 }
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,58 +1,53 @@
 package personnages;
 
 public class Gaulois {
-	
-	private String nom;
+    private String nom;
     private int force;
-    private int effetPotion;
-    private int force, nb_trophees;
-    private Equipement trophees[] = new Equipement[100];
-    
+    private int effetPotion = 1;
+
     public Gaulois(String nom, int force) {
-    	this.nom = nom;
-    	this.force = force;
+        this.nom = nom;
+        this.force = force;
     }
 
     public String getNom() {
-    	return nom;
+        return nom;
     }
-    
-    public void parler(String texte) {
-    	System.out.println(prendreParole() + "« " + texte + "»");
-    }
-    
-    private String prendreParole() {
-    	String texte = "Le gaulois " + nom + " : ";
-    	return texte;
-    	}
-    
-    public void frapper(Romain romain) {
-    	System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-    	romain.recevoirCoup(force / 3);
-    }
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
-	public static void main(String[] args) {
-		//TODO créer un main permettant de tester la classe Gaulois
-		Gaulois asterix = new Gaulois("Astérix", 8);
-		System.out.println(asterix);
-		System.out.println(asterix.prendreParole());
-		asterix.parler("GLORY TO UKRAINE");
-		Romain rom = new Romain("Roma", 5);
-		asterix.frapper(rom);
 
-	}
-	
-	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans lamâchoire de " + romain.getNom());
-		Equipement trophees[] = romain.recevoirCoup((force / 3) *
-		effetPotion);
-		for (int i = 0; trophees != null && i < trophees.length; i++,
-		nb_trophees++) {
-		this.trophees[nb_trophees] = trophees[i];
-		}
-		return;
-		}
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "« " + texte + "»");
+    }
+
+    private String prendreParole() {
+        return "Le gaulois " + nom + " : ";
+    }
+
+    public void frapper(Romain romain) {
+        System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+        romain.recevoirCoup(force / 3 * effetPotion);
+    }
+
+    @Override
+    public String toString() {
+        return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+    }
+
+    public void boirePotion(int forcePotion) {
+        effetPotion = forcePotion;
+        parler("Merci Druide, je sens que ma force est " + effetPotion + " fois décuplée.");
+    }
+
+    public static void main(String[] args) {
+        Gaulois asterix = new Gaulois("Asterix", 8);
+        System.out.println(asterix);
+        
+        // Test boirePotion
+        asterix.boirePotion(5);
+        System.out.println(asterix);
+        
+        // Test frapper after drinking potion
+        Romain romain = new Romain("Julius Caesar", 10);
+        asterix.frapper(romain);
+    }
 }
+
